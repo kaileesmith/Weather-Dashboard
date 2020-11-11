d94378579ce94faee6d2f5dce12fd8157dd16715
Kailee Smith <kaileesmith1501@gmail.com>
2020-11-10 22:53:32 -0800

    adding 5 day forecast start and finished current forecast

--- Assets/script.js
+++ Assets/script.js
@@ -10,15 +10,14 @@ var apiKey = "&appid=b63a4d4a08ab8180b775d0f57ddfe7a5";
 $('#searchBtn').on('click', function(e){
     var city = document.getElementById('searchOption').value
     var searchCity = []
-    var listItem = $("<li>").addClass("list-group-item").text(city);
+    // var listItem = $("<li>").addClass("list-group-item").text(city); SPLIT STRING
     e.preventDefault();
 
     var pastCity = JSON.parse(localStorage.getItem("city"));
     if(pastCity){
         pastCity.map(item => searchCity.push(item));
         searchCity.push(city)
-        searchCity.map( item => document.getElementById('pastCities').append(listItem))
-        // append("<li>" + item + "</li>"))
+        searchCity.map( item => document.getElementById('pastCities'). append("<li>" + item + "</li>"))
         localStorage.setItem("city", JSON.stringify(searchCity))
     } else{
         searchCity.push(city)
@@ -45,7 +44,13 @@ function showCurrent(city){
         console.log(data)
 })
     
-// window.localStorage.setItem( city , temp)
+var multiDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
+
+    fetch(multiDayUrl)
+    // .then(data => data.json())
+    .then(data => { 
+        console.log(data)
+    });
 };
 
 
@@ -53,18 +58,34 @@ function pullWeather( d ) {
 
     var weathericon= d.weather[0].icon
     var fahrenheit = Math.round((d.main.temp)-273.15)*1.8+32
+    var date = new Date;
+
     
-    document.getElementById('currentCity').innerText = d.weather[0].description;
+    // document.getElementById('currentCity').innerText = d.weather[0].description;
 	document.getElementById('temp').innerHTML = fahrenheit  + '&deg;';
     document.getElementById('location').innerText = d.name;
-    document.getElementById('wind').innerHTML = d.wind.speed;
+    document.getElementById('wind').innerHTML = "Wind Speed: " + d.wind.speed;
     document.getElementById('icon').src = "http://openweathermap.org/img/wn/" + weathericon + "@2x.png"
+    document.getElementById('humidity').innerText = "Humidity: " + d.main.humidity + "%";
+    document.getElementById('currentDate').innerHTML = date
+    document.getElementById('forecast5D').innerHTML = d.dt
+
+    for (let i = 6; i < forecast5D.length; i++) {
+        
+    }
+
     };
 
+// 5 day weather
 
-// var weathericon= data.weather[0].icon;
-// var date = new Date(data.dt*1000).toLocaleDateString();
+// function show5Day(d){
+//     var multiDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
 
-// $('#currentCity').html(data.name +"("+date+")");
-
-
+//     fetch(multiDayUrl)
+//     // .then(data => data.json())
+//     .then(data => { 
+//         console.log(data)
+//     })
+//     document.getElementById('forecast5D').innerHTML = d.dt
+    
+// }
